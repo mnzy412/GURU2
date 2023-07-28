125acd24f9e94585545ab09a48bb38e8d0d48412
mnzy412 <mnzy412@gmail.com>
2023-07-29 02:16:26 +0900

    [mnzy] 리사이클러뷰 완료 (디테일 수정 필요)

--- app/src/main/java/com/example/myapplication/mypage/MypageFragment.kt
+++ app/src/main/java/com/example/myapplication/mypage/MypageFragment.kt
@@ -1,64 +1,74 @@
 package com.example.myapplication.mypage
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.view.GravityCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.myapplication.MypageRVAdpater
 import com.example.myapplication.R
+import com.example.myapplication.databinding.FragmentMypageBinding
 
-
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
+@Suppress("UNREACHABLE_CODE")
 class MypageFragment : Fragment() {
-
-    private var param1: String? = null
-    private var param2: String? = null
-
-    var UserList = arrayListOf<User>(
-
-        User("보관된 책","korean-otter@naver.com"),
-        User("완독한 책","jmssk11@naver.com"),
-        User("읽은 시간","hello@gmail.com"),
-        User("물고기","asdf@naver.com")
-    )
-    //배열을 만들어 사용자의 정보를 담아준다.
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_mypage)
-
-        var Adapter = ListAdapter(this, UserList)
-        item_list.adapter = Adapter
-        //만들어둔 ListAdapter를 이용해 카드뷰 - 사용자 정보 - 리스트뷰를 연결시켜준다.
-    }
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var viewBinding: FragmentMypageBinding
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
+
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mypage, container, false)
-    }
+        viewBinding = FragmentMypageBinding.inflate(layoutInflater)
 
-    companion object {
+        val mypageList: ArrayList<Mypage> = arrayListOf()
+        val mAdapter = MypageRVAdpater(mypageList)
 
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MypageFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        mypageList.apply {
+            add(
+                Mypage(
+                    "보관된 책",
+                    3,
+                    "권"
+                )
+            )
+            add(
+                Mypage(
+                    "완독한 책",
+                    27,
+                    "권"
+                )
+            )
+            add(
+                Mypage(
+                    "독서 시간",
+                    375,
+                    "시간"
+                )
+            )
+            add(
+                Mypage(
+                    "메모",
+                    32,
+                    "개"
+                )
+            )
+        }
+
+        viewBinding.mypageRv.layoutManager = GridLayoutManager (context,2)
+        viewBinding.mypageRv.adapter = mAdapter
+
+
+//        mAdapter!!.itemClick = object : MypageRVAdpater.ItemClick {
+//
+//        }
+        return viewBinding.root
+
     }
 }
+
--- app/src/main/java/com/example/myapplication/mypage/Mypage_info.kt
+++
@@ -1,6 +0,0 @@
-package com.example.myapplication.mypage
-
-class mypage_info {
-    val title: String,
-    val sub: String
-}
