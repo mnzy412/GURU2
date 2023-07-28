e6f3569086dca7655da5fe541fdbdb81ca76f50b
mnzy412 <mnzy412@gmail.com>
2023-07-28 16:59:10 +0900

    [mnzy] 로그인/회원가입 완료

---
+++ app/src/main/java/com/example/myapplication/Signup2Activity.kt
@@ -0,0 +1,80 @@
+package com.example.myapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.example.myapplication.databinding.ActivitySignup2Binding
+import com.example.myapplication.databinding.ActivitySignupBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+
+class Signup2Activity : AppCompatActivity() {
+
+    lateinit var viewBinding: ActivitySignup2Binding
+
+    lateinit var mAuth: FirebaseAuth
+
+    private lateinit var mDbRef: DatabaseReference
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewBinding = ActivitySignup2Binding.inflate(layoutInflater)
+
+
+        val nickname = viewBinding.nicknameEt.text.toString()
+        val email = intent.getStringExtra("email").toString()
+        val password = intent.getStringExtra("password").toString()
+
+        Log.d("Signup2Activity", "nickname: ${nickname}")
+        Log.d("Signup2Activity", "password: ${password}")
+
+
+        //인증 초기화
+        mAuth = Firebase.auth
+
+        //db 초기화
+        mDbRef = Firebase.database.reference
+
+        viewBinding.signupFinBtn.setOnClickListener {
+
+            signup(nickname, email, password)
+
+        }
+
+        setContentView(viewBinding.root)
+    }
+
+    private fun addUserToDatabase(nickname: String,email: String, uId: String){
+        mDbRef.child("user").child(uId).setValue(User(nickname,email,uId))
+    }
+
+
+    //회원가입
+    private fun signup(nickname:String, email:String, password:String){
+        mAuth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {  //회원가입 성공
+                    val intent = Intent (this, LoginActivity::class.java)
+                    overridePendingTransition(0, 0) //화면 전환 효과 없애기
+                    startActivity(intent)
+                    Toast.makeText(this, "회원가입 성공", Toast.LENGTH_SHORT,).show()
+                    addUserToDatabase(nickname,email, mAuth.currentUser?.uid!!)
+
+                } else {  //회원가입 실패
+                    Toast.makeText(this, "회원가입 실패", Toast.LENGTH_SHORT,).show()
+
+                    Log.d("register","Error: ${task.exception}")
+
+                }
+            }
+    }
+
+
+
+}
+
