138e588b1c5be12f5090d67d52ddc642dd45f6a8
mnzy412 <mnzy412@gmail.com>
2023-07-30 01:45:51 +0900

    [mnzy]

--- app/src/main/java/com/example/myapplication/mypage/Mypage_quit.kt
+++ app/src/main/java/com/example/myapplication/mypage/Mypage_quit.kt
@@ -5,6 +5,7 @@ import com.google.firebase.auth.FirebaseAuth
 import android.os.Bundle
 import android.widget.Button
 import com.example.myapplication.LoginActivity
+import com.example.myapplication.R
 import com.example.myapplication.Signup2Activity
 import com.google.firebase.database.DatabaseReference
 
@@ -16,18 +17,18 @@ class Mypage_quit: AppCompatActivity() {
         setContentView(R.layout.mypage_quit)
     }
 
-    val deleteBtn = findViewById<Button>(R.id.btn_delete)
-        deleteBtn.setOnClickListener {
-
-        val nickname = viewBinding.nicknameEt.text.toString()
-        val email = intent.getStringExtra("email").toString()
-        val password = intent.getStringExtra("password").toString()
-
-        val signup2Activity = Signup2Activity.getInstance(this, "mDbRef.db",)
-
-        dbHelper.delete(nickname)
-        dbHelper.delete(email)
-        dbHelper.delete(password)
-
-        }
+//    val deleteBtn = findViewById<Button>(R.id.btn_delete)
+//        deleteBtn.setOnClickListener {
+//
+//        val nickname = viewBinding.nicknameEt.text.toString()
+//        val email = intent.getStringExtra("email").toString()
+//        val password = intent.getStringExtra("password").toString()
+//
+//        val signup2Activity = Signup2Activity.getInstance(this, "mDbRef.db",)
+//
+//        dbHelper.delete(nickname)
+//        dbHelper.delete(email)
+//        dbHelper.delete(password)
+//
+//        }
 }
