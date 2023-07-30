adcd7f902dda7b4d9e35c6df47b89d86f82ef50b
lynyn <taelyn312@gmail.com>
2023-07-30 22:03:43 +0900

    [lyn] mypage xml 완성

--- app/src/main/java/com/example/myapplication/mypage/MypageFragment.kt
+++ app/src/main/java/com/example/myapplication/mypage/MypageFragment.kt
@@ -1,37 +1,35 @@
 package com.example.myapplication.mypage
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.Toast
-import androidx.core.view.GravityCompat
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.myapplication.LoginActivity
 import com.example.myapplication.MypageRVAdpater
 import com.example.myapplication.R
 import com.example.myapplication.databinding.FragmentMypageBinding
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+
 
 @Suppress("UNREACHABLE_CODE")
 class MypageFragment : Fragment() {
     private lateinit var viewBinding: FragmentMypageBinding
-    lateinit var Auth: FirebaseAuth
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+    lateinit var mypage_btn_quit: Button
+    lateinit var mypage_btn_change: Button
+    lateinit var mypage_btn_logout: Button
 
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?)
+    : View? {
         viewBinding = FragmentMypageBinding.inflate(layoutInflater)
-        Auth = Firebase.auth
-
 
         val mypageList: ArrayList<Mypage> = arrayListOf()
         val mAdapter = MypageRVAdpater(mypageList)
@@ -71,23 +69,59 @@ class MypageFragment : Fragment() {
         viewBinding.mypageRv.adapter = mAdapter
 
 
+        mypage_btn_logout = findViewbyId(R.id.mypage_btn_logout)
+        mypage_btn_change = findViewbyId(R.id.mypage_btn_change)
+        mypage_btn_quit = findViewbyId(R.id.mypage_btn_quit)
+
+        //로그아웃 버튼 클릭 시 다이얼로그창 띄우기
+        mypage_btn_logout.setOnClickListener{view ->
+            var dialog = AlertDialog.Builder(this)
+            dialog.setTitle("로그아웃")
+            dialog.setMessage("Booktory를 정말 로그아웃하시겠습니까?")
+            dialog.setIcon(R.drawable.mypage_profill)
+
+            FirebaseAuth.getInstance().signOut()
+
+            fun toast_p() {
+                Toast.makeText(view.context, "로그아웃 되었습니다.", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this,LoginActivity::class.java)
+                startActivity(intent)
+            }
+            var dialog_listener = object: DialogInterface.OnClickListener{
+                override fun onClick(dialog: DialogInterface?, which: Int) {
+                    when(which){
+                        DialogInterface.BUTTON_POSITIVE ->
+                            toast_p()
+                    }
+                }
+            }
+            dialog.setPositiveButton("네",dialog_listener)
+            dialog.setNegativeButton("아니오",null)
+            dialog.show()
+        }
+        mypage_btn_change.setOnClickListener{
+
+            val intent = Intent(this, Mypage_pwch::class.java)
+            startActivity(intent)
+
+        }
+        mypage_btn_quit.setOnClickListener{
+
+            val intent = Intent(this, Mypage_quit::class.java)
+            startActivity(intent)
+
+        }
+
 //        mAdapter!!.itemClick = object : MypageRVAdpater.ItemClick {
 //
 //        }
-
-        viewBinding.mypageBtnLogout.setOnClickListener(){
-            Firebase.auth.signOut()
-            var intent=Intent(context, LoginActivity::class.java) //로그인 페이지 이동
-            startActivity(intent)
-            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-        }
-
-
-
         return viewBinding.root
 
     }
 
+    private fun Intent(mypageFragment: MypageFragment, java: Class<Mypage_quit>): Intent? {
+
+    }
 
 }
 
