7a7ca84c7d90443324eec4694f43fa3544ddd893
김예원 <flower_d_o@naver.com>
2023-07-31 00:11:39 +0900

    책 검색 웹뷰 추가 - 네이티브, 웹뷰 간 로그인 유지 코드 추가

--- app/src/main/java/com/example/myapplication/utils/GuruJavaScriptInterface.kt
+++ app/src/main/java/com/example/myapplication/utils/GuruJavaScriptInterface.kt
@@ -8,51 +8,16 @@ import android.webkit.JavascriptInterface
 
 class GuruJavaScriptInterface (
     private val activity: Activity,
+    private val currentUserUid: String,
 ) {
-
+    @JavascriptInterface
+    fun getUserUid(): String{
+        return currentUserUid
+    }
 
     @JavascriptInterface
     fun close(){
         activity.finish()
     }
-//
-//    @JavascriptInterface
-//    fun getUserPosition() {
-//        log.d("getUserPosition")
-//        var address = mainActivity.getAddress()
-//
-//        val gson: Gson? = Gson()
-//        val point = Point(address[0], address[1])
-//        val result = gson?.toJson(point)
-//        log.d("getUserPosition : $result")
-//        handler.post {
-//            mainActivity.webView.loadUrl("javascript:window.apps.getUserPosition(`${result}`)")
-//        }
-//    }
-//
-//    @JavascriptInterface
-//    fun getVersionName(): String {
-//        log.d("getVersionName")
-//        return BuildConfig.VERSION_NAME // ex) 2.2.8
-//    }
-//
-//    @JavascriptInterface
-//    fun downloadFiles(files: String) {
-//        val gson = Gson()
-//        try {
-//            val files = gson.fromJson(files, Array<BobaFile>::class.java)
-//            files.forEach { file ->
-//                val request = DownloadManager.Request(Uri.parse(file.url))
-//                    .setTitle(file.name)
-//                    .setDescription("다운로드중입니다...")
-//                    // 다운로드 진행중, 완료일 때 알림에 표시
-//                    .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
-//                    .setAllowedOverMetered(true)
-//                    .setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, file.name)
-//                downloadManager.enqueue(request)
-//            }
-//        } catch (e: JsonParseException) {
-//            Toast.makeText(context, "파일 다운로드를 할 수 없습니다, JSON 변환에 실패했습니다.", Toast.LENGTH_SHORT).show()
-//        }
-//    }
+
 }
