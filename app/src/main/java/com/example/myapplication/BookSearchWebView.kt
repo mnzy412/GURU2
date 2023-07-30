1961a3aca95e06f0e5fb3dbdce82b63435d313e3
김예원 <flower_d_o@naver.com>
2023-07-31 00:11:39 +0900

    책 검색 웹뷰 추가 - 네이티브, 웹뷰 간 로그인 유지 코드 추가

--- app/src/main/java/com/example/myapplication/BookSearchWebView.kt
+++ app/src/main/java/com/example/myapplication/BookSearchWebView.kt
@@ -5,17 +5,31 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.webkit.WebSettings
 import android.webkit.WebView
+import android.widget.Toast
 import com.example.myapplication.utils.GuruJavaScriptInterface
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class BookSearchWebView : AppCompatActivity() {
 
+    // private val URL = "http://10.0.2.2:5173/book-search"
     private val URL = "https://outsourcing-guru-book.vercel.app/book-search"
     private lateinit var webView: WebView
+
+    lateinit var auth: FirebaseAuth
 
     @SuppressLint("JavascriptInterface")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_book_search_web_view)
+
+        auth = Firebase.auth
+        if(auth.currentUser == null){
+            Toast.makeText(this, "로그인이 필요한 페이지입니다", Toast.LENGTH_SHORT).show()
+            finish()
+            return
+        }
 
         webView = findViewById(R.id.bookSearchWebView)
 
@@ -31,8 +45,10 @@ class BookSearchWebView : AppCompatActivity() {
         settings.setSupportMultipleWindows(false)
         settings.setSupportZoom(false)
 
-        // 여기서 설장한 이름을 통하여 JavaScript 에서 호출할 수 있게 된다.
-        webView.addJavascriptInterface(GuruJavaScriptInterface(this), "AndroidGuru2")
+        webView.addJavascriptInterface(
+            GuruJavaScriptInterface(this, auth.currentUser!!.uid),
+            "AndroidGuru2"
+        )
 
         webView.loadUrl(URL)
     }
