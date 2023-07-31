8d9d80d45713325395e4c25a550e72069abfcf50
mnzy412 <mnzy412@gmail.com>
2023-07-31 21:34:52 +0900

    [mnzy] 탈퇴 코드 작성

--- app/src/main/java/com/example/myapplication/mypage/MypageQuitActivity.kt
+++ app/src/main/java/com/example/myapplication/mypage/MypageQuitActivity.kt
@@ -1,11 +1,10 @@
 package com.example.myapplication.mypage
 
-import android.content.Intent.FLAG_ACTIVITY_CLEAR_TOP
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import android.os.Bundle
 import android.widget.Button
-import com.example.myapplication.R
 import com.example.myapplication.databinding.ActivityMypageQuitBinding
 import com.google.firebase.database.DatabaseReference
 
@@ -22,14 +21,19 @@ class MypageQuitActivity: AppCompatActivity() {
         var viewBinding = ActivityMypageQuitBinding.inflate(layoutInflater)
 
        viewBinding.quitClose.setOnClickListener() {
-           finish()
+           val intent = Intent(this, MypageFragment::class.java)
+           startActivity(intent)
+           finish() // 현재 액티비티를 종료하여 이전 액티비티로 돌아가는 효과를 낼 수 있습니다.
        }
 
-        btn_delete.setOnClickListener{
-            mAuth.signOut()
-            startActivity(intent.addFlags(FLAG_ACTIVITY_CLEAR_TOP))
-            finish()
+        viewBinding.btnNext.setOnClickListener{
+            val intent = Intent(this, MypageRealQuitActivity::class.java)
+            this.startActivity(intent)
+            overridePendingTransition(0, 0) //화면 전환 효과 없애기
         }
+
+
+        setContentView(viewBinding.root)
     }
 
 
